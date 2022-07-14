fd4b57e58485259c1267385452c7fe1a87462e70
PaperGroceries <106277251+PaperGroceries@users.noreply.github.com>
2022-07-13 19:46:14 -0700

    started rational class

--- Ass2/Ass2/main.cpp
+++ Ass2/Ass2/main.cpp
@@ -6,9 +6,51 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+
+class RationalNumber
+{
+    int a;
+    int b;
+public:
+    RationalNumber();
+    RationalNumber(int divident);
+    RationalNumber(int divident, int divisor);
+    int add();
+    void less();
+    double mul();
+    double sub();
+    double div();
+    void input();
+    void output();
+    //considering turning into string since the division is not carried out
+    
+};
+RationalNumber::RationalNumber()
+{
+    a = 0;
+    b = 1;
+};
+RationalNumber::RationalNumber(int divident)
+{
+    a = divident;
+    b = 1;
+};
+RationalNumber::RationalNumber(int divident, int divisor)
+{
+    a = divident;
+    b = divisor;
+    if(b < 0)
+    {
+        b *= -1;
+        a *= -1;
+    }
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
+    
 }
